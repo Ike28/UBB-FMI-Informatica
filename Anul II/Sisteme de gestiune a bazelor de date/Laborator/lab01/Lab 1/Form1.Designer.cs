d3b4fc18f0a7d82f5c4e27ec8cc3d0e80aa9a594
Mihai Corocaescu <mihai.corocaescu2@gmail.com>
2023-04-11 14:48:40 +0300

    sgbd lab01

---
+++ Anul II/Sisteme de gestiune a bazelor de date/Laborator/lab01/Lab 1/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿
+namespace Lab_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewParent = new System.Windows.Forms.DataGridView();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.dataGridViewSon = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.idBox = new System.Windows.Forms.TextBox();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.phoneBox = new System.Windows.Forms.TextBox();
+            this.emailBox = new System.Windows.Forms.TextBox();
+            this.idLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.phoneLabel = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewParent
+            // 
+            this.dataGridViewParent.BackgroundColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.dataGridViewParent.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridViewParent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewParent.Location = new System.Drawing.Point(32, 29);
+            this.dataGridViewParent.Name = "dataGridViewParent";
+            this.dataGridViewParent.RowTemplate.Height = 25;
+            this.dataGridViewParent.Size = new System.Drawing.Size(603, 300);
+            this.dataGridViewParent.TabIndex = 0;
+            this.dataGridViewParent.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridViewParent_CellMouseDoubleClick);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(81, 486);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(174, 71);
+            this.buttonRefresh.TabIndex = 1;
+            this.buttonRefresh.Text = "Refresh";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // dataGridViewSon
+            // 
+            this.dataGridViewSon.BackgroundColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.dataGridViewSon.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridViewSon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewSon.Location = new System.Drawing.Point(742, 29);
+            this.dataGridViewSon.Name = "dataGridViewSon";
+            this.dataGridViewSon.RowTemplate.Height = 25;
+            this.dataGridViewSon.Size = new System.Drawing.Size(579, 300);
+            this.dataGridViewSon.TabIndex = 2;
+            this.dataGridViewSon.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridViewSon_CellMouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Snow;
+            this.label1.Location = new System.Drawing.Point(290, 373);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sedii";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Snow;
+            this.label2.Location = new System.Drawing.Point(1010, 373);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Agenti";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // idBox
+            // 
+            this.idBox.Location = new System.Drawing.Point(1358, 38);
+            this.idBox.Name = "idBox";
+            this.idBox.Size = new System.Drawing.Size(182, 23);
+            this.idBox.TabIndex = 5;
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(1358, 103);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(182, 23);
+            this.nameBox.TabIndex = 6;
+            // 
+            // phoneBox
+            // 
+            this.phoneBox.Location = new System.Drawing.Point(1358, 172);
+            this.phoneBox.Name = "phoneBox";
+            this.phoneBox.Size = new System.Drawing.Size(182, 23);
+            this.phoneBox.TabIndex = 7;
+            // 
+            // emailBox
+            // 
+            this.emailBox.Location = new System.Drawing.Point(1358, 243);
+            this.emailBox.Name = "emailBox";
+            this.emailBox.Size = new System.Drawing.Size(182, 23);
+            this.emailBox.TabIndex = 8;
+            // 
+            // idLabel
+            // 
+            this.idLabel.AutoSize = true;
+            this.idLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.idLabel.ForeColor = System.Drawing.Color.Snow;
+            this.idLabel.Location = new System.Drawing.Point(1412, 64);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(82, 25);
+            this.idLabel.TabIndex = 9;
+            this.idLabel.Text = "ID Sediu";
+            this.idLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.nameLabel.ForeColor = System.Drawing.Color.Snow;
+            this.nameLabel.Location = new System.Drawing.Point(1417, 129);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(63, 25);
+            this.nameLabel.TabIndex = 10;
+            this.nameLabel.Text = "Nume";
+            this.nameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // phoneLabel
+            // 
+            this.phoneLabel.AutoSize = true;
+            this.phoneLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.phoneLabel.ForeColor = System.Drawing.Color.Snow;
+            this.phoneLabel.Location = new System.Drawing.Point(1412, 198);
+            this.phoneLabel.Name = "phoneLabel";
+            this.phoneLabel.Size = new System.Drawing.Size(73, 25);
+            this.phoneLabel.TabIndex = 11;
+            this.phoneLabel.Text = "Telefon";
+            this.phoneLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.emailLabel.ForeColor = System.Drawing.Color.Snow;
+            this.emailLabel.Location = new System.Drawing.Point(1422, 269);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(58, 25);
+            this.emailLabel.TabIndex = 12;
+            this.emailLabel.Text = "Email";
+            this.emailLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.emailLabel.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(1358, 306);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(182, 47);
+            this.updateButton.TabIndex = 13;
+            this.updateButton.Text = "Actualizare";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(1358, 373);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(182, 47);
+            this.deleteButton.TabIndex = 14;
+            this.deleteButton.Text = "Stergere";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(1358, 440);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(182, 47);
+            this.addButton.TabIndex = 15;
+            this.addButton.Text = "Adaugare";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1552, 631);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.phoneLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.idLabel);
+            this.Controls.Add(this.emailBox);
+            this.Controls.Add(this.phoneBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.idBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewSon);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.dataGridViewParent);
+            this.Name = "Form1";
+            this.Text = "Sal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDoubleClick);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewParent;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.DataGridView dataGridViewSon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox idBox;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox phoneBox;
+        private System.Windows.Forms.TextBox emailBox;
+        private System.Windows.Forms.Label idLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label phoneLabel;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button addButton;
+    }
+}
+
---
+++ Anul II/Sisteme de gestiune a bazelor de date/Laborator/lab01/Lab 1/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Lab_1
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Server=DESKTOP-UM07TFE;Initial Catalog=DiebmannImobiliare;Integrated Security=true;";
+        DataSet ds = new DataSet();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlDataAdapter sonAdapter = new SqlDataAdapter();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void hideStuff()
+        {
+            label2.Visible = false;
+            emailBox.Visible = false;
+            emailLabel.Visible = false;
+            phoneBox.Visible = false;
+            phoneLabel.Visible = false;
+            nameBox.Visible = false;
+            nameLabel.Visible = false;
+            idBox.Visible = false;
+            idLabel.Visible = false;
+            addButton.Visible = false;
+            deleteButton.Visible = false;
+            updateButton.Visible = false;
+            //dataGridViewSon.Visible = false;
+        }
+
+        private void clearBoxes()
+        {
+            idBox.Clear();
+            nameBox.Clear();
+            phoneBox.Clear();
+            emailBox.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using(SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    adapter.SelectCommand = new SqlCommand("SELECT * FROM Sedii", connection);
+                    adapter.Fill(ds, "Sedii");
+                    dataGridViewParent.DataSource = ds.Tables["Sedii"];
+                    hideStuff();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using(SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    adapter.SelectCommand.Connection = connection;
+                    ds.Tables["Sedii"].Clear();
+                    ds.Tables["Agenti"].Clear();
+                    adapter.SelectCommand = new SqlCommand("SELECT * FROM Sedii;", connection);
+                    adapter.Fill(ds, "Sedii");
+                    sonAdapter.SelectCommand = new SqlCommand("SELECT * FROM Agenti", connection);
+                    sonAdapter.Fill(ds, "Agenti");
+                    dataGridViewParent.DataSource = ds.Tables["Sedii"];
+                    dataGridViewSon.DataSource = ds.Tables["Agenti"];
+                    hideStuff();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dataGridViewParent_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            try
+            {
+                using(SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    dataGridViewSon.Visible = true;
+                    adapter.SelectCommand.Connection = connection;
+                    adapter.SelectCommand = new SqlCommand("SELECT * FROM Agenti", connection);
+                    if (ds.Tables["Agenti"] != null)
+                        ds.Tables["Agenti"].Clear();
+                    adapter.Fill(ds, "Agenti");
+                    dataGridViewSon.DataSource = ds.Tables["Agenti"];
+                    label2.Visible = true;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dataGridViewSon_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            emailBox.Visible = true;
+            emailLabel.Visible = true;
+            phoneBox.Visible = true;
+            phoneLabel.Visible = true;
+            nameBox.Visible = true;
+            nameLabel.Visible = true;
+            idBox.Visible = true;
+            idLabel.Visible = true;
+            addButton.Visible = true;
+            deleteButton.Visible = true;
+            updateButton.Visible = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("UPDATE Agenti SET nume=@nume, idSediuAsignat=@idSediu, telefon=@telefon, email=@email WHERE idAgent = @id", cn))
+                {
+                    if (cn.State == ConnectionState.Closed)
+                        cn.Open();
+
+                    DataGridViewRow row = dataGridViewSon.Rows[dataGridViewSon.CurrentRow.Index] as DataGridViewRow;
+                    if (row != null
+                        && (idBox.Text.Length != 0
+                        || nameBox.Text.Length != 0
+                        || phoneBox.Text.Length != 0
+                        || emailBox.Text.Length != 0))
+                    {
+                        cmd.Parameters.AddWithValue("@id", row.Cells["idAgent"].Value);
+                        if (idBox.Text.Length != 0)
+                        {
+                            cmd.Parameters.AddWithValue("@idSediu", idBox.Text);
+                        }
+                        else
+                        {
+                            cmd.Parameters.AddWithValue("@idSediu", row.Cells["idSediuAsignat"].Value);
+                        }
+
+                        if (nameBox.Text.Length != 0)
+                        {
+                            cmd.Parameters.AddWithValue("@nume", nameBox.Text);
+                        }
+                        else
+                        {
+                            cmd.Parameters.AddWithValue("@nume", row.Cells["nume"].Value);
+                        }
+
+                        if (phoneBox.Text.Length != 0)
+                        {
+                            cmd.Parameters.AddWithValue("@telefon", phoneBox.Text);
+                        }
+                        else
+                        {
+                            cmd.Parameters.AddWithValue("@telefon", row.Cells["telefon"].Value);
+                        }
+
+                        if (emailBox.Text.Length != 0)
+                        {
+                            cmd.Parameters.AddWithValue("@email", emailBox.Text);
+                        }
+                        else
+                        {
+                            cmd.Parameters.AddWithValue("@email", row.Cells["email"].Value);
+                        }
+
+                        cmd.ExecuteNonQuery();
+                        clearBoxes();
+                        buttonRefresh_Click(sender, e);
+                        MessageBox.Show("Agent modificat.", "Succes!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Cel putin un camp trebuie modificat pentru actualizare", "Error");
+
+                    }
+                }
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow row = dataGridViewSon.Rows[dataGridViewSon.CurrentRow.Index] as DataGridViewRow;
+            if (row != null)
+            {
+                if (MessageBox.Show("Doriti sa stergeti acest agent?", "Atentie", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    using (SqlConnection cn = new SqlConnection(connectionString))
+                    {
+                        if (cn.State == ConnectionState.Closed)
+                            cn.Open();
+                        using (SqlCommand cmd = new SqlCommand("DELETE FROM Agenti WHERE idAgent = @id", cn))
+                        {
+                            cmd.Parameters.AddWithValue("id", row.Cells["idAgent"].Value);
+                            cmd.ExecuteNonQuery();
+                            clearBoxes();
+                            buttonRefresh_Click(sender, e);
+                            MessageBox.Show("Agent sters.", "Succes!");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                if (cn.State == ConnectionState.Closed)
+                    cn.Open();
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO Agenti(idSediuAsignat,nume,telefon,email) VALUES(@idSediu,@nume,@telefon,@email)", cn))
+                {
+                    if (nameBox.Text.Length != 0
+                        && phoneBox.Text.Length != 0
+                        && emailBox.Text.Length != 0)
+                    {
+                        DataGridViewRow row = dataGridViewParent.Rows[dataGridViewParent.CurrentRow.Index] as DataGridViewRow;
+
+                        cmd.Parameters.AddWithValue("@idSediu", row.Cells["idSediu"].Value);
+                        cmd.Parameters.AddWithValue("@nume", nameBox.Text);
+                        cmd.Parameters.AddWithValue("@telefon", phoneBox.Text);
+                        cmd.Parameters.AddWithValue("@email", emailBox.Text);
+                        cmd.ExecuteNonQuery();
+                        clearBoxes();
+                        buttonRefresh_Click(sender, e);
+                        MessageBox.Show("Agent adaugat.", "Succes!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Adaugarea nu a putut avea loc:\n   * Toate campurile trebuiesc completate", "Eroare");
+                    }
+                }
+            }
+        }
+    }
+}
